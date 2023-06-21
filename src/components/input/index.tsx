19597a88573603c5f4b3a9b25d4ce1c57adcc12a
Felipe Fernandes <felipekurosaki123@gmail.com>
2023-06-20 22:22:42 -0300

    adjust height input

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -6,7 +6,7 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 
 export function Input({ icon, ...props }: InputProps) {
   return (
-    <div className="border-0 h-9  bg-zinc-800 mb-3 px-2 flex flex-row items-center justify-between rounded-md ">
+    <div className="border-0 h-10  bg-zinc-800 mb-3 px-2 flex flex-row items-center justify-between rounded-md ">
       <input
         className=" w-full h-full outline-none px-2 bg-zinc-800 text-white"
         {...props}
