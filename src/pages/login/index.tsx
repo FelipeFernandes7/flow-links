e749b8b3e645edaed13b0c35aa16b3314e29d292
Felipe Fernandes <felipekurosaki123@gmail.com>
2023-06-20 22:59:59 -0300

    adjust layout

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -5,6 +5,7 @@ import { MdOutlineMailOutline } from "react-icons/md";
 import { RiLockPasswordLine } from "react-icons/ri";
 import { auth } from "../../services/firebaseConnection";
 import { signInWithEmailAndPassword } from "firebase/auth";
+import { toast } from "react-toastify";
 
 export function Login() {
   const [email, setEmail] = useState("");
@@ -14,20 +15,24 @@ export function Login() {
   function handleSubmit(e: FormEvent) {
     e.preventDefault();
     if (email === "" || password === "") {
-      alert("preencha todos os campos");
+      toast.warn("preencha todos os campos", { theme: "light" });
       return;
     }
     signInWithEmailAndPassword(auth, email, password)
       .then(() => {
+        toast.success("Login efetuado com sucesso", {
+          position: "bottom-center",
+        });
         navigate("/admin", { replace: true });
       })
       .catch((error) => {
         console.log(error, "erro ao logar");
+        toast.error("Houve um erro de autenticação");
       });
   }
   return (
-    <div className="flex w-full h-screen items-center justify-center flex-col">
-      <Link to={"/"}>
+    <div className="flex w-full h-screen items-center flex-col">
+      <Link to={"/"} className="mt-4">
         <h1 className=" text-white mb-7 font-bold text-5xl">
           Flow
           <span className="bg-gradient-to-r from-blue-600 to-violet-500 bg-clip-text text-transparent">
@@ -37,7 +42,7 @@ export function Login() {
       </Link>
       <form
         onSubmit={handleSubmit}
-        className="w-full max-w-sm flex flex-col px-4"
+        className="w-full max-w-sm flex flex-col px-4 h-full justify-center"
       >
         <Input
           placeholder="Digite seu email"
@@ -47,7 +52,7 @@ export function Login() {
           icon={<MdOutlineMailOutline color={"#cec9c9"} size={16} />}
         />
         <Input
-          placeholder="**********"
+          placeholder="Digite sua senha"
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
@@ -55,7 +60,7 @@ export function Login() {
         />
         <button
           type="submit"
-          className=" mt-4 h-9 bg-gradient-to-r from-blue-600 to-violet-500 
+          className="md:h-12 mt-4 h-12 bg-gradient-to-r from-blue-600 to-violet-500 
           rounded border-0 text-lg font-medium text-white transition-all active:scale-95"
         >
           Entrar
